35f8d1ee8d21f271986f90070ecccca653d83378
Seohyun <63396820+jejuvita01@users.noreply.github.com>
2024-02-06 13:03:17 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 268 ms, Memory: 8572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(vector<int> a, vector<int> b)
+{
+    if (a[0] < b[0])
+        return true;
+    else if (a[0] == b[0])
+        return a[1] < b[1];
+    else
+        return false;
+}
+
+int main(void)
+{
+    int n;
+    vector<vector<int>> point;
+    
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        vector<int> temp;
+        cin >> x >> y;
+        temp.push_back(x);
+        temp.push_back(y);
+        point.push_back(temp);
+    }
+    
+    sort(point.begin(), point.end(), comp);
+    
+    for (int i = 0; i < n; i++) {
+        cout << point[i][0] << ' ' << point[i][1] << '\n';
+    }
+    
+    return 0;
+}
