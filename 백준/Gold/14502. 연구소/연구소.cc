a96add843b109fd29a90a6d3369cf4a478528382
Seohyun <63396820+jejuvita01@users.noreply.github.com>
2024-06-26 18:01:45 +0900

    [Gold IV] Title: 연구소, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,139 +1,138 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cmath>
+#include <deque>
+#include <vector>
 
 using namespace std;
 
 int n, m;
-vector<vector<int>> visited(8, vector<int>(8, 0));
-vector<int> dfs_map[8];
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
-int answer = 0;
-
-
-void print_map(vector<int> map[])
-{
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << map[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-}
+int map[8][8];
+int new_map[8][8];
+int visited[8][8];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 
 void init_visited(void)
 {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
             visited[i][j] = 0;
-    
-    return;
 }
 
-void dfs(vector<int> map[], int x, int y) // 감염시키며 탐색
+void get_map_info(void)
 {
-    if (visited[x][y] == 1) return;
+    cin >> n >> m;
     
-    map[x][y] = 2;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> map[i][j];
+        }
+    }
+}
+
+void copy_map(void)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            new_map[i][j] = map[i][j];
+}
+
+bool is_inside(int x, int y)
+{
+    return (0 <= x && x < n && 0 <= y && y < m);
+}
+
+void dfs(int arr[8][8], int x, int y)
+{
+    arr[x][y] = 2;
     visited[x][y] = 1;
     
     for (int i = 0; i < 4; i++) {
         int new_x = x + dx[i];
         int new_y = y + dy[i];
-        if (0 <= new_x && new_x < n && 0 <= new_y && new_y < m) {
-            if ((map[new_x][new_y] == 2 || map[new_x][new_y] == 0) && visited[new_x][new_y] == 0) {
-                dfs(map, new_x, new_y);
-            }
-            else
-                visited[new_x][new_y] = 1;
-        }
-    }
-}
-
-int cnt_safe(vector<int> map[]) // 안전한 위치 개수 세기
-{
-    int count = 0;
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (map[i][j] == 0)
-                count++;
-        }
-    }
-    
-    return count;
-}
-
-void copy_map(vector<int> origin[], vector<int> dest[]) // dest에 origin의 값 복사
-{
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            dest[i][j] = origin[i][j];
-        }
-    }
-}
-
-void make_wall(vector<int> new_map[], int wall) // 재귀로 벽 세개 세우기
-{
-    if (wall == 3) {
-        init_visited();
-        copy_map(new_map, dfs_map);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (dfs_map[i][j] == 2 && visited[i][j] == 0) {
-                    dfs(dfs_map, i, j);
-                }
-            }
-        }
-        answer = max(cnt_safe(dfs_map), answer);
-        return;
-    }
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (new_map[i][j] == 0) {
-                new_map[i][j] = 1;
-                make_wall(new_map, wall + 1);
-                new_map[i][j] = 0;
+        if (is_inside(new_x, new_y)) {
+            if (arr[new_x][new_y] != 1 && visited[new_x][new_y] == 0) {
+                dfs(arr, new_x, new_y);
             }
         }
     }
 }
 
+void spread_virus(int arr[8][8])
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == 2 && visited[i][j] == 0)
+                dfs(arr, i, j);
+        }
+    }
+}
+
+int cnt_safe(int arr[8][8])
+{
+    int cnt = 0;
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == 0)
+                cnt++;
+        }
+    }
+    
+    return cnt;
+}
+
+void print_map(int arr[8][8]) {
+    cout << "\n MAP \n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
 
 int main(void)
 {
-    cin >> n >> m;
-    vector<int> original_map[n];
-    vector<int> new_map[n];
-    int wall;
+    int max_safe = 0;
     
-    for (int i = 0; i < n; i++) {
-        int x;
-        for (int j = 0; j < m; j++) {
-            cin >> x;
-            original_map[i].push_back(x);
-            new_map[i].push_back(x);
-            dfs_map[i].push_back(x);
-        }
-    }
+    get_map_info();
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (original_map[i][j] == 0) {
-                wall = 1;
-                init_visited();
-                copy_map(original_map, new_map);
-                new_map[i][j] = 1;
-                make_wall(new_map, 1);
-                new_map[i][j] = 0;
+    for (int i1 = 0; i1 < n; i1++) {
+        for (int j1 = 0; j1 < m; j1++) {
+            if (map[i1][j1] == 0) {
+                for (int i2 = i1; i2 < n; i2++) {
+                    for (int j2 = 0; j2 < m; j2++) {
+                        if (map[i2][j2] == 0) {
+                            if (i2 == i1 && j2 <= j1)
+                                continue;
+                            else {
+                                for (int i3 = i2; i3 < n; i3++) {
+                                    for (int j3 = 0; j3 < m; j3++) {
+                                        if (map[i3][j3] == 0) {
+                                            if (i3 == i2 && j3 <= j2)
+                                                continue;
+                                            else {
+                                                copy_map();
+                                                new_map[i1][j1] = 1;
+                                                new_map[i2][j2] = 1;
+                                                new_map[i3][j3] = 1;
+                                                init_visited();
+                                                spread_virus(new_map);
+                                                max_safe = max(max_safe, cnt_safe(new_map));
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
     
-    cout << answer << '\n';
+    cout << max_safe << '\n';
     
     return 0;
 }
